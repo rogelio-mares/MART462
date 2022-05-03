4f743ae38dad8e48c5793480b0eaeef04070b9b5
rogelio-mares <77692713+rogelio-mares@users.noreply.github.com>
2022-05-03 01:45:14 -0600

    Add files via upload

---
+++ milestone 5/pacer/js/styles.js
@@ -0,0 +1,13 @@
+//fixed NAV BAR
+var topnav = document.getElementById("topnav");
+var sticky = topnav.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
+
+
+
