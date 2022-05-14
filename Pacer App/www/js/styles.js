3651b28550f050219eb0c6a36ba551666117bb87
rogelio-mares <77692713+rogelio-mares@users.noreply.github.com>
2022-05-13 21:14:28 -0600

    Add files via upload

---
+++ Pacer App/www/js/styles.js
@@ -0,0 +1,30 @@
+//fixed NAV BAR
+var topnav = document.getElementById("topnav");
+var sticky = topnav.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
